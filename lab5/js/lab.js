c0e04ada97c6fba3a33390f79db3bb1673ebb1a0
jonathancastanon <jcastanon@csumb.edu>
2022-02-10 15:51:15 -0800

    update

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+//Author: Jonathan Castanon
+//Date:2/10/2022
+
+
+//Define Variables
+make = "Chevy";
+model = "Impala";
+color = "white";
+year = "2003";
+
+//calculate
+
+age = 2022 - year;
+
+//Output
+
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
